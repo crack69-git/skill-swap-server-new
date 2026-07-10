0f42970ff8fc2077695bc31652e092ec2751974c
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-07-10 23:47:08 +0600

    added post a task

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 import dns from "dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
+const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri);
 
-const db = client.db(process.env.MONGODB_DB_NAME);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-export async function connectToMongoDB() {
+async function run() {
   try {
-    await client.connect();
-    console.log("You successfully connected to MongoDB!");
-    return client;
+    // await client.connect();
+    const database = client.db(process.env.MONGODB_DB_NAME);
+    const tasksCollection = database.collection("tasks");
+
+    app.post("/api/task/post", async (req, res) => {
+      const task = req.body;
+      const result = await tasksCollection.insertOne(task);
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } catch (err) {
-    console.error(err);
+    console.error("MongoDB connection error:", err);
   }
 }
+run();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Skillswap API!");
+  res.send("Hello World!");
 });
-export async function disconnectFromMongoDB() {
-  await client.close();
-}
-
-connectToMongoDB();
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
